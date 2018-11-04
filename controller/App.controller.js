d70142b25770209d551591bc64cc8ebfad28d7f2
S0018321251 <gyurkan.yilmaz@kaufland.com>
2018-11-04 20:32:28 +0000

    add generic tile

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	'sap/ui/core/mvc/Controller',
+	'sap/m/MessageToast'
+], function (Controller,MessageToast) {
 	"use strict";
 
 	return Controller.extend("ems.UI5Showcase.controller.App", {
-
+		press: function (evt) {
+			MessageToast.show("The GenericTile is pressed.");
+		}
 	});
 });
