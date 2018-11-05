5d7aa1b26b5cd715b821cb2636156d9bdb775c80
S0018321251 <gyurkan.yilmaz@kaufland.com>
2018-11-05 09:14:43 +0000

    i18n_en file & app url

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,15 @@
 sap.ui.define([
 	'sap/ui/core/mvc/Controller',
 	'sap/m/MessageToast'
-], function (Controller,MessageToast) {
+], function (Controller, MessageToast) {
 	"use strict";
+	//url for the repo
+	const url = 'https://yoncacoskun.github.io';
 
 	return Controller.extend("ems.UI5Showcase.controller.App", {
+		onInit: function () {},
 		press: function (evt) {
-			MessageToast.show("Tile is pressed.");
+			window.open(url);
 		}
 	});
 });
