da83a3ae9d4df1307fb3573451c558c63ee6297c
S0018321251 <gyurkan.yilmaz@kaufland.com>
2018-11-08 10:15:14 +0000

    NF

--- controller/BaseController.js
+++ controller/BaseController.js
@@ -28,6 +28,32 @@ sap.ui.define([
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 				oRouter.navTo("indexTiles", {}, true);
 			}
-		}
+		},
+		getModel: function (name) {
+			return this.getView().getModel(name) || this.getOwnerComponent().getModel(name);
+		},
+
+		setModel: function (model, name) {
+			return this.getView().setModel(model, name);
+		},
+		getEventBus: function () {
+			return this.getOwnerComponent().getEventBus();
+		},
+
+		getResourceBundle: function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+
+		getListSelector: function () {
+			return this.getOwnerComponent().listSelector;
+		},
+
+		getMessageManager: function () {
+			return sap.ui.getCore().getMessageManager();
+		},
+
+		getMessageManagerUtils: function () {
+			return this.getOwnerComponent()._messageManagerUtils;
+		},
 	});
 });
