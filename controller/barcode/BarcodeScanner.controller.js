9b29840e75e7773576970c6e742b5123c25e7668
Gurkan Yilmaz <gurkanyilmaz@Gurkans-MacBook-Pro.local>
2019-02-14 15:10:02 +0200

    Barcode Scanner Decoder Types

--- controller/barcode/BarcodeScanner.controller.js
+++ controller/barcode/BarcodeScanner.controller.js
@@ -77,10 +77,7 @@ sap.ui.define([
           numOfWorkers: 2,
           frequency: 10,
           decoder: {
-            readers: [{
-              format: "code_128_reader",
-              config: {}
-            }]
+            readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
           },
           locate: true
         }, function (error) {
