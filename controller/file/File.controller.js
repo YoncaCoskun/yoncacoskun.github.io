1b409231d86ba0b3a6dde1025633abf2045a0f32
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-03-21 19:33:50 +0200

    Uploader update

--- controller/file/File.controller.js
+++ controller/file/File.controller.js
@@ -1,64 +1,57 @@
-sap.ui.define([
-		"ems/UI5Showcase/controller/BaseController",
-		'sap/m/MessageToast'
-	],
-	function (BaseController, MessageToast) {
-		"use strict";
-		return BaseController.extend("ems.UI5Showcase.controller.file.File", {
-			onInit: function () {
-				this.getView().byId("html").setContent("<div id='textGrid' width='400' height='200' ></div>");
-			},
-			onAfterRendering: function () {
-
-			},
-			processFiles: function (data) {
-				return new Promise(function (res, rej) {
-
-				});
-			},
-			handleUploadPress: function () {
-				var oUploader = this.getView().byId("fileUploader");
-				var domRef = oUploader.getFocusDomRef();
-				var file = domRef.files[0];
-				var reader = new FileReader();
-				reader.readAsBinaryString(file);
-				var data = reader.result;
-				var fileContent = XLSX.read(data, {
-					type: "binary"
-				});
-				var excelContent = fileContent.Strings;
-				if (excelContent !== undefined) {
-					delete excelContent.Unique;
-					delete excelContent.Count;
-					var parentNode = document.getElementById("textGrid");
-					var grid = canvasDatagrid({
-						parentNode: parentNode,
-						data: excelContent
-					});
-				} else {
-					console.error('Can\'t read the file!');
-				}
-
-
-				/* var grid = canvasDatagrid({
-					parentNode: parentNode,
-					data: [{
-							col1: 'foo',
-							col2: 0,
-							col3: 'a'
-						},
-						{
-							col1: 'bar',
-							col2: 1,
-							col3: 'b'
-						},
-						{
-							col1: 'baz',
-							col2: 2,
-							col3: 'c'
-						}
-					]
-				}); */
-			}
-		});
-	});
+sap.ui.define(
+  ["ems/UI5Showcase/controller/BaseController", "sap/m/MessageToast"],
+  function(BaseController, MessageToast) {
+    "use strict";
+    return BaseController.extend("ems.UI5Showcase.controller.file.File", {
+      onInit: function() {
+        this.getView()
+          .byId("html")
+          .setContent("<div id='textGrid' width='400' height='200' ></div>");
+      },
+      onAfterRendering: function() {},
+      processFiles: function(data) {
+        return new Promise(function(res, rej) {});
+      },
+      handleUploadPress: function() {
+        var fileUpload = this.getView().byId("fileUploader");
+        var domRef = fileUpload.getFocusDomRef();
+        var file = domRef.files[0];
+        if (file === undefined) {
+          var ss = 0;
+        } else {
+          this.importExcelFile(file);
+        }
+      },
+      importExcelFile: function(file) {
+        var that = this;
+        if (file && window.FileReader) {
+          //Initialize Reader
+          var reader = new FileReader();
+          var result = {};
+          var data;
+          reader.onload = function(e) {
+            data = e.target.result;
+            //get workbook data as binary
+            var wb = XLSX.read(data, {
+              type: "binary"
+            });
+            wb.SheetNames.forEach(function(sheetName) {
+              var roa = XLSX.utils.sheet_to_row_object_array(
+                wb.Sheets[sheetName]
+              );
+              if (roa.length > 0) {
+                result[sheetName] = roa;
+              }
+              var parentNode = document.getElementById("textGrid");
+              var grid = canvasDatagrid({
+                parentNode: parentNode,
+                data: roa
+              });
+            });
+          };
+          reader.readAsBinaryString(file);
+        }
+      }
+    });
+  }
+);
